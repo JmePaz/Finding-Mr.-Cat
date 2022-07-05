76a3e3db3b6b79335091961c94d2023dbad4492e
James Tiger <jmepaz@gmail.com>
2022-07-05 18:11:23 +0800

    Collision Handler
    Implement a Collision Handler on rocket game object

---
+++ Assets/Scripts/RocketCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) {
+        GameObject otherGObj = other.gameObject;
+        if(otherGObj.tag == "Finish"){
+            Debug.Log("Successes:Planet 1 is finished.");
+        }
+        else if(otherGObj.tag == "Obstacles"){
+             Debug.Log("Hitted an Obstacle.");
+        }
+    }
+}
